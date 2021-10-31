7f8666852cf809d4c113c4e4b63bcca38492995c
MisterJS <iammaindiana@gmail.com>
2021-10-30 19:04:45 -0500

    aws, mail,tokens

--- src/routers/v1/user.router.js
+++ src/routers/v1/user.router.js
@@ -8,9 +8,11 @@ module.exports = () =>{
     router.get('/generate-access-token', user.getAccessToken);
     router.post('/login',vertifyTokenMiddleware('access'),user.login)
     router.post('/veriphy-phone',vertifyTokenMiddleware('access'),user.sendVerifyPhone)
+    router.post('/send-password-recovery-code',vertifyTokenMiddleware('access'),user.sendPasswordRecoveryCode)
     router.post('/veriphy-email',vertifyTokenMiddleware('access'),user.sendVerifyEmail)
     router.get('/get-profile',vertifyTokenMiddleware('auth'),user.getProfile)
     router.delete('/disable-account',vertifyTokenMiddleware('auth'),user.disableAccount)
-    router.put('/update-profile/:id',vertifyTokenMiddleware('auth'),user.updateProfile)
+    router.put('/update-profile',vertifyTokenMiddleware('auth'),user.updateProfile)
+    router.put('/update-password',vertifyTokenMiddleware('access'),user.updatePassword)
     return router
 }
